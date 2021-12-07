0d2098f9e966028af761dadc385c77a3d69ac0ec
David Skrovanek <58830926+djskrovanek@users.noreply.github.com>
2021-12-07 10:44:14 -0600

    edits

--- designPIcontroller.m
+++ designPIcontroller.m
@@ -8,22 +8,21 @@ function [Kp, Ki, A_aug, B_aug, C_aug, D_aug] = designPIcontroller()
 B_red = B(:, 2:5); % B with reduced set of actuators u2:u5
 % u1 and u6 are disturbances
 
-% an arbitrary set of poles to place
-p = [-10 -10 -10 -1 -500];
-
-[Kp,prec] = place(A,B_red,p); % place poles
-
 % design of integrator
 A_int = [[A, [0, 0, 0, 0, 0]']; [0, 0, 0, 1, 0, 0]];
 B_int = [B_red; [0, 0, 0, 0]];
 C_int = [[C, [0, 0, 0, 0, 0, 0]']];
 D_int = D;
 
+% an arbitrary set of poles to place
+p = [-10 -10 -10 -1 -500, -10];
+[Kp,prec] = place(A,B_int,p); % place poles
+
 % augmented system has 2 dimensional input u_aug for disturbances
 % with state equation x_dot = A_aug*x+B_aug*u_aug
 
-A_aug = A_int-B_red*Kp; % A matrix for augmented system with feedback law
-B_aug = [B(:, 1), B(:, 6)]; % B matrix for augmented system
+B_aug = [[B(:, 1);0], B_int, [B(:, 6);0]]; % B matrix for augmented system
+A_aug = A_int-B_aug*Kp; % A matrix for augmented system with feedback law
 C_aug = C_int-D_int(:,2:5)*Kp;
 D_aug = [D_int(:,1), D_int(:,6)];
 
